50fdf04a2251a555dacf26ef6962d87da67e4f4e
Alexandr Semak <semakaleksandr2014@gmail.com>
2019-01-24 12:18:01 +0200

    Delete config.js

--- src/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const FirebaseConfig = {
-  apiKey: "AIzaSyD5l2TLEwpZw_Mm7UZKeMLi-Ru26FtBeM0",
-  authDomain: "workout-ec6f3.firebaseapp.com",
-  databaseURL: "https://workout-ec6f3.firebaseio.com",
-  projectId: "workout-ec6f3",
-  storageBucket: "workout-ec6f3.appspot.com",
-  messagingSenderId: "691298428964"
-};
