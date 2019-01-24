93e9e45d53b312fc17eac75a77a67df73cc93050
Kamesas <semakaleksandr2014@gmail.com>
2019-01-24 13:17:35 +0200

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { connect } from "react-redux";
 import { addExample } from "./store/actions/exampleAction";
-import Example from "./components/Example";
+import WorkOut from "./components/workOut/WorkOut";
 
 class App extends Component {
   actionExample = () => {
@@ -14,7 +14,7 @@ class App extends Component {
       <div className="App">
         <h1>Start template create-react-app</h1>
         {`Store === ${this.props.example}`}
-        <Example />
+        <WorkOut />
         <button onClick={this.actionExample}>Action example</button>
       </div>
     );
---
+++ src/components/workOut/WorkOut.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class WorkOut extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h4>WorkOut</h4>
+      </div>
+    );
+  }
+}
+
+export default WorkOut;
