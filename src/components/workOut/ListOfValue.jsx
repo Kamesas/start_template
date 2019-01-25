7e48d4389ebe880df341fac6d6fcec7475dc79af
Kamesas <semakaleksandr2014@gmail.com>
2019-01-25 19:25:36 +0200

    added TotalValue

--- src/components/workOut/ListOfValue.jsx
+++ src/components/workOut/ListOfValue.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import { List } from "semantic-ui-react";
 import ItemOfValue from "./ItemOfValue";
+import TotalValue from "./TotalValue";
 
-const ListOfValue = () => (
-  <List horizontal>
-    <List.Item>
-      <ItemOfValue />
-    </List.Item>
-    <List.Item>
-      <ItemOfValue />
-    </List.Item>
-  </List>
-);
+class ListOfValue extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <List horizontal>
+          <List.Item>
+            <ItemOfValue />
+          </List.Item>
+          <List.Item>
+            <ItemOfValue />
+          </List.Item>
+        </List>
+        <div>
+          <TotalValue />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ListOfValue;
---
+++ src/components/workOut/TotalValue.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Statistic } from "semantic-ui-react";
+
+const TotalValue = () => (
+  <Statistic>
+    <Statistic.Value>155</Statistic.Value>
+    <Statistic.Label>Приседаний</Statistic.Label>
+  </Statistic>
+);
+
+export default TotalValue;
