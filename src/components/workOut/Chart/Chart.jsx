b3f7aa3a22042bd45862a184a281de9232922df3
Kamesas <semakaleksandr2014@gmail.com>
2019-02-04 11:14:11 +0200

    component Chart ready

--- src/components/workOut/Chart/Chart.jsx
+++ src/components/workOut/Chart/Chart.jsx
@@ -1,89 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import { Line } from "react-chartjs-2";
-//import { data } from "./dateForChart";
-import moment from "moment";
-//import "moment/locale/ru";
-import _ from "lodash";
-//import DateSwitch from "../../HOC/DateSwitch";
+import { monthName } from "./allMonthValue";
+import { datasetsForChartDate } from "./dateForChart";
 
-class Chart extends Component {
-  getAllDaysOfMonth = monthNum => {
-    const m = moment();
-    let daysOfMonth = [];
-    const endMonth = m
-      .month(monthNum)
-      .endOf("month")
-      .date();
-    let i = 0;
-    while (i < endMonth) {
-      daysOfMonth.push(
-        m
-          .month(monthNum)
-          .startOf("month")
-          .add(i, "day")
-          .format("DD MM YYYY")
-      );
-      i++;
-    }
-
-    return daysOfMonth;
+const Chart = ({ workoutValues }) => {
+  const data = {
+    labels: monthName,
+    datasets: [
+      datasetsForChartDate("присед", "blue", workoutValues),
+      datasetsForChartDate("подтягивания", "red", workoutValues),
+      datasetsForChartDate("отжимания", "grey", workoutValues)
+    ]
   };
-
-  totalMonthValue = (i, exercise = "присед", workoutValues = {}) => {
-    let totalMonth = [];
-    this.getAllDaysOfMonth(i).map(value => {
-      return _.map(workoutValues, v =>
-        v.date === value && v.exercise === exercise
-          ? totalMonth.push(+v.numberOfTimes)
-          : []
-      );
-    });
-    return totalMonth.length > 0 ? totalMonth.reduce((f, l) => f + l) : 0;
-  };
-
-  AllMonthValue = (exercise = "присед", workoutValues = {}) => {
-    let totalAllMonth = [];
-    for (let i = 0; i < 12; i++) {
-      totalAllMonth.push(this.totalMonthValue(i, exercise, workoutValues));
-    }
-    return totalAllMonth;
-  };
-
-  render() {
-    const monthName = [
-      "Январь",
-      "Февраль",
-      "Март",
-      "Апрель",
-      "Мая",
-      "Июнь",
-      "Июль",
-      "Август",
-      "Сентябрь",
-      "Октябрь",
-      "Ноябрь",
-      "Декабрь"
-    ];
-
-    const data = {
-      labels: monthName,
-      datasets: [
-        {
-          label: "Присед",
-          fill: false,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
-          data: this.AllMonthValue("присед", this.props.workoutValues)
-        }
-      ]
-    };
-
-    return (
-      <div>
-        <Line data={data} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Line data={data} />
+    </div>
+  );
+};
 
 export default Chart;
--- src/components/workOut/Chart/dateForChart.js
+++ src/components/workOut/Chart/dateForChart.js
@@ -1,47 +1,15 @@
-export const data = {
-  labels: ["January", "February", "March", "April", "May", "June", "July"],
-  datasets: [
-    {
-      label: "Присед",
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: "rgba(75,192,192,0.4)",
-      borderColor: "rgba(75,192,192,1)",
-      borderCapStyle: "butt",
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: "miter",
-      pointBorderColor: "rgba(75,192,192,1)",
-      pointBackgroundColor: "#fff",
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(75,192,192,1)",
-      pointHoverBorderColor: "rgba(220,220,220,1)",
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 110, 11]
-    },
-    {
-      label: "Подтягивания",
-      fill: false,
-      lineTension: 0.3,
-      backgroundColor: "rgba(0,0,0,0.4)",
-      borderColor: "rgba(75,192,192,1)",
-      borderCapStyle: "butt",
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: "miter",
-      pointBorderColor: "rgba(0,0,0,1)",
-      pointBackgroundColor: "#eee",
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(0,0,0,1)",
-      pointHoverBorderColor: "rgba(220,220,220,1)",
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [35, 49, 70, 41, 96, 45, 10]
-    }
-  ]
+import { allMonthValue } from "./allMonthValue";
+
+export const datasetsForChartDate = (
+  exercise = "присед",
+  color = "rgba(75,192,192,0.4)",
+  workoutValues = {}
+) => {
+  return {
+    label: exercise,
+    fill: false,
+    backgroundColor: color,
+    borderColor: color,
+    data: allMonthValue(exercise, workoutValues)
+  };
 };
