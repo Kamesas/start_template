49171430eba61c32eadb621bac755bacb8d11697
Kamesas <semakaleksandr2014@gmail.com>
2019-02-04 10:22:34 +0200

    Chart component ready

--- src/components/workOut/Chart/Chart.jsx
+++ src/components/workOut/Chart/Chart.jsx
@@ -7,8 +7,6 @@ import _ from "lodash";
 //import DateSwitch from "../../HOC/DateSwitch";
 
 class Chart extends Component {
-  state = { m: moment(), totelValueMonthAquare: [] };
-
   getAllDaysOfMonth = monthNum => {
     const m = moment();
     let daysOfMonth = [];
@@ -31,24 +29,22 @@ class Chart extends Component {
     return daysOfMonth;
   };
 
-  totalMonthValue = i => {
+  totalMonthValue = (i, exercise = "присед", workoutValues = {}) => {
     let totalMonth = [];
-
     this.getAllDaysOfMonth(i).map(value => {
-      return _.map(this.props.workoutValues, v =>
-        v.date === value && v.exercise === "присед"
+      return _.map(workoutValues, v =>
+        v.date === value && v.exercise === exercise
           ? totalMonth.push(+v.numberOfTimes)
           : []
       );
     });
-
     return totalMonth.length > 0 ? totalMonth.reduce((f, l) => f + l) : 0;
   };
 
-  AllMonthValue = () => {
+  AllMonthValue = (exercise = "присед", workoutValues = {}) => {
     let totalAllMonth = [];
     for (let i = 0; i < 12; i++) {
-      totalAllMonth.push(this.totalMonthValue(i));
+      totalAllMonth.push(this.totalMonthValue(i, exercise, workoutValues));
     }
     return totalAllMonth;
   };
@@ -77,7 +73,7 @@ class Chart extends Component {
           fill: false,
           backgroundColor: "rgba(75,192,192,0.4)",
           borderColor: "rgba(75,192,192,1)",
-          data: this.AllMonthValue()
+          data: this.AllMonthValue("присед", this.props.workoutValues)
         }
       ]
     };
